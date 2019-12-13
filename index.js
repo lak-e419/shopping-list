1f3c4d07c7078c6821eb4e104d211328c482b4a7
lak-e419 <54508955+lak-e419@users.noreply.github.com>
2019-12-12 23:06:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -14,20 +14,6 @@ function getItem (present, itemIndex) {
 function deleteItem (present, itemIndex) {
   present.items.splice[itemIndex, 1];
 }
-function updateItem (present, itemIndex, newItem) {
-  present.items[itemIndex] = newItem;
-}
-function renderItem (item, itemId, itemTemplate, itemDataAttr){
-  var element=$(itemTemplate);
-  element.find('.js-shopping-item').text(item.displayName);
-  if (item.checkedOff) {
-  element.find('.js-shopping-item').addClass('shopping-item_checked');
-  }
-  element.find('.js-shopping-item-toggle')
-  element.attr(itemDataAttr, itemId);
-  return element;
-}
-
 function handleItemAdd (
  formElement, newItemIdentifier, itemDataAttr, listElement, present) {
   formElement.submit(function(event) {
@@ -58,32 +44,7 @@ function handleItemDelete (
       renderList(present, listElement, itemDataAttr);
     })
   }
-$(function() {
-  var formElement = $('#js-shopping-list-form');
-  var listElement = $('.js-shopping-list');
-  var newItemIdentifier = 'js-new-item';
-  var itemDataAttr = 'data-list-item-id';
-  var toggleIdentifier = 'js-shopping-item-toggle'
-  handleItemAdd(
-    formElement, newItemIdentifier,itemDataAttr, listElement, present);
-  handleItemDelete(
-    formElement, removeIdentifier, itemDataAttr, listElement, present);
-  handleItemToggle(
-    listElement, toggleIdentifier, itemDataAttr, itemDataAttr, present);
-});
-var listItemTemplate = (
-  '<li>' +
-    '<span class="shopping-item js-shopping-item"></span>' +
-    '<div class="shopping-item-controls">' +
-      '<button class="js-shopping-item-toggle">' +
-        '<span class="button-label">check</span>' +
-      '</button>' +
-      '<button class="js-shopping-item-delete">' +
-        '<span class="button-label">delete</span>' +
-      '</button>' +
-    '</div>' +
-  '</li>'
-); 
+
 
 
   
